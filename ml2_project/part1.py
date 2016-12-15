1a618f5de13f1c228d243b0c6b35eb7d217f6fec
JINGXU <walter.1218@hotmail.com>
2016-12-15 17:03:23 +0800

    update

--- ml2_project/part1.py
+++ ml2_project/part1.py
@@ -77,7 +77,26 @@ def pcs(x, y):
     """
     Insert your code here.
     """
-    return 0.
+    A = utility[x - 1]
+    B = utility[y - 1]
+    I = [ (rxi,ryi) for (rxi,ryi) in zip(A,B) if rxi>0 and ryi>0  ]
+    #print(I)
+    if len(I)>0:
+        pa = 0
+        pb = 0
+        sum_pab = 0
+        sum_pyz = 0
+        for (rxi,ryi) in I:
+            #print rxi,ryi
+            pa = (rxi - user[x - 1].avg_r)
+            pb = (ryi - user[y - 1].avg_r)
+            sum_pab += pa * pb
+            sum_pyz += (pa * pa) * (pb * pb)
+        #print(sum_pab,sum_pyz)
+        results = sum_pab/sum_pyz
+    else:
+        results = 0
+    return results
 
 # Guesses the ratings that user with id, user_id, might give to item with id, i_id.
 # We will consider the top_n similar users to do this. Use top_n as 3 in this example.
@@ -85,6 +104,24 @@ def guess(user_id, i_id, top_n):
     """
     Insert your code here.
     """
+    n_users = utility.shape[0]
+    pcs_matrix = np.zeros((n_users, n_users))
+    for i in range(0, n_users):
+        for j in range(0, n_users):
+            pcs_matrix[i][j] = pcs(i , j )
+    similarity_list = []
+    #print(n_users)
+    for i in range(0, n_users):
+        if i != (user_id-1):
+            #print("infor: ",user_id-1, i)
+            #print("infor",pcs_matrix[user_id - 1][i])
+            similarity_list.append((pcs_matrix[user_id - 1][i],i + 1))
+    #print(similarity_list)
+    #print("the input data is",user_id, i_id, top_n)
+    similarity_list.sort(key=lambda x:x[0],reverse=True)
+    similarity_list = similarity_list[:top_n]
+    print(similarity_list)
+    #TODO FINISHE HERE
     return 0.
 
 """
